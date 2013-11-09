30bf1faafc001be16d2b9eb43c3599aefbaa49ae
Michael <michael.molho@gmail.com>
2013-11-09 14:48:21 +0100

    First version

---
+++ shazam2dl.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python2.6
+# -*- coding: utf-8 -*- 
+
+import urllib
+import HTMLParser
+import urllib2
+import re
+import sys
+import json
+import os
+import time
+import subprocess
+import shazam_api
+import smtplib
+from os import listdir, remove
+from os.path import isfile, join, getsize
+
+if os.path.ismount("/data/musique/files/Musique") == False:
+    sys.exit(1)
+
+dl_dir = "/data/musique/files/Musique/"
+fb_login = sys.argv[1]
+fb_pass = sys.argv[2]
+
+uid_cookie, fat_cookie = shazam_api.get_api_cookie(fb_login, fb_pass)
+html_parser = HTMLParser.HTMLParser()
+already_dl = [ f for f in listdir(dl_dir) if isfile(join(dl_dir,f)) ]
+FNULL = open('/dev/null' , 'w')
+
+def add_proper_headers(http_req, accept, referer, cookie = ""):
+    http_req.add_header('User-Agent', "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0")
+    http_req.add_header('Accept', accept)
+    http_req.add_header('Accept-Language', "fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+    http_req.add_header('Connection', "keep-alive")
+    http_req.add_header('Referer', referer)
+    if cookie != "":
+        http_req.add_header('Cookie', cookie)
+
+
+def get_shazam_tags(fat_cookie, uid_cookie):
+    tags = []
+    req = urllib2.Request("http://www.shazam.com/fragment/myshazam?size=large")
+    add_proper_headers( req, 
+                        accept = "application/json, text/javascript, */*; q=0.01",
+                        referer = "http://www.shazam.com/myshazam",
+                        cookie = "fat=" + fat_cookie + "; uid=" + uid_cookie + ";"
+                      )
+    resp = urllib2.urlopen(req)
+    content = json.loads(resp.read())['feed'].split('\n')
+
+    i = 0
+    for line in content:
+        if len(re.findall('tg-title">(.*)<', line)) > 0:
+            title = re.findall('url">(.*)</a', content[i])[0]
+            artist = re.findall('tg-artist">(.*)<', content[i+1])[0]
+            filename = html_parser.unescape(title + '-' + artist + ".mp3")
+            if { 'artist' : artist, 'title' : title } not in tags and filename not in already_dl:
+                tags += [{ 'artist' : artist, 'title' : title, 'filename' : filename }]
+        i += 1
+    return tags
+    
+def get_youtube_links(title, artist):
+    links = []
+    youtube_search = "http://gdata.youtube.com/feeds/api/videos?q=" + urllib.quote(title + " " + artist) + "&v=2&max-results=4&start-index=1&category=Music&format=5&fields=entry(id,title,link[@rel=%27alternate%27],author(name),yt:statistics,media:group(media:content),media:group(media:thumbnail))&alt=json-in-script&callback=define&key=AI39si6WqJb0Pbzi0lVyf3RMFS0DKg23xVBcBXDwnkzZGEMWP_JKsMQ47-6P8rV4BRkre3YNjhvXuSJ6dWeInyweJ2vmlOBJtw".encode('utf-8')
+
+    req = urllib2.Request(youtube_search)
+    add_proper_headers(req, "*/*", "http://www.shazam.com/myshazam")
+    resp = urllib2.urlopen(req)
+    json_resp = json.loads( resp.readlines()[1][7:-2] )
+    for entry in json_resp['feed']['entry']:
+        links += [entry['link'][0]['href']]
+    return links
+
+def try_dl_mp3_1(youtube_link, filename):
+    dl_link = "http://youtubeinmp3.com/fetch/?video=" + youtube_link
+    print "Try to download from " + dl_link + " ... "
+    req = urllib2.Request(dl_link)
+    add_proper_headers( req,
+                        accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                        referer = "http://youtubeinmp3.com/api"
+                      )
+    resp = urllib2.urlopen(req)
+    fd = open(dl_dir + filename, "w")
+    fd.write(resp.read())
+    fd.flush()
+    fd.close()
+    filesize = getsize(dl_dir + filename)
+    if filesize < 1048576:
+        remove(dl_dir + filename)    
+        return False
+    else:
+        return True
+
+def try_dl_mp3_2(youtube_link, filename):
+    convert_url = "http://www.vidtomp3.com/cc/conversioncloud.php"
+    post_data = { 'mediaurl' : youtube_links }
+    post_data = urllib.urlencode(post_data)
+    req = urllib2.Request(convert_url)
+    add_proper_headers( req,
+                        accept = "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+                        referer = "http://www.vidtomp3.com/process.php"
+                      )
+    resp = urllib2.urlopen(req, post_data)
+    server, video_id, key = re.findall('([a-zA-Z0-9]+).vidtomp3.com.*\?videoid=([0-9]+)&key=(.*)"', resp.read())[0]
+
+    status_url = "http://"+server+".vidtomp3.com/api/?videoid="+video_id+"&key=" + key
+    req = urllib2.Request(status_url)
+    resp = urllib2.urlopen(req)
+    dl_url = re.findall('<downloadurl><\!\[CDATA\[(.*)/\]\]></downloadurl>', resp.read())[0]
+
+    dl_url = urllib.unquote(dl_url)
+    print "Try to download from " + dl_url + " ... "
+    req = urllib2.Request(dl_url)
+    resp = urllib2.urlopen(req)
+
+    fd = open(dl_dir + filename, "w")
+    fd.write(resp.read())
+    fd.flush()
+    fd.close()
+    filesize = getsize(dl_dir + filename)
+    if filesize < 1048576:
+        remove(dl_dir + filename)    
+        return False
+    else:
+        return True
+
+def sendemail(artist, titre):
+    sender = 'molho.michael@orange.fr'
+    receivers = ['michael.molho@gmail.com']
+
+    message =  "From: shazam2dl <shazam2dl@orange.fr>\n"
+    message += "To: michael <michael.molho@gmail.com>\n"
+    message += "Subject: [shazam2dl] Nouveau morceau telecharge\n\n"
+    message += artist + " : " + titre
+
+    try:
+        smtpObj = smtplib.SMTP('smtp.orange.fr')
+        smtpObj.sendmail(sender, receivers, message)         
+        print "Email successfuly sent !"
+    except SMTPException:
+        print "Error: unable to send email"
+    
+
+
+tags = get_shazam_tags(fat_cookie, uid_cookie)
+
+for tag in tags:
+    dl_result = False
+    title = unicode(html_parser.unescape(tag['title'])).encode('utf-8')
+    artist = unicode(html_parser.unescape(tag['artist'])).encode('utf-8')
+
+    print "######## " + title + " -- " + artist
+
+    youtube_links = get_youtube_links(title, artist)
+
+    for link in youtube_links:
+        dl_result = try_dl_mp3_1( link, tag['filename'] )
+        if dl_result == True:
+            break
+
+    if dl_result == False:
+        for link in youtube_links:
+            dl_result = try_dl_mp3_2( link, tag['filename'] )
+            if dl_result == True:
+                break
+
+    if dl_result == True:
+        subprocess.Popen(["/usr/local/bin/eyeD3", "--v1", "-a", artist, "-t", title, dl_dir + tag['filename']], stdout=FNULL, stderr=FNULL)
+        subprocess.Popen(["/usr/local/bin/eyeD3", "--v2", "-a", artist, "-t", title, dl_dir + tag['filename']], stdout=FNULL, stderr=FNULL)
+        #subprocess.Popen(["chown", "www-data:www-data", dl_dir + tag['filename']]) 
+        #sendemail(artist, title)
+    else:
+        print "FAILED !!"
---
+++ shazam_api.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*- 
+
+import urllib
+import HTMLParser
+import urllib2
+import re
+import sys
+import json
+import os
+import cookielib
+
+global cookies 
+global user_id
+global access_token
+html_parser = HTMLParser.HTMLParser()
+
+
+
+class MyHTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+    def http_error_302(self, req, fp, code, msg, headers):
+        global cookies
+        global user_id
+        global access_token
+       
+        if "https://www.facebook.com/dialog/oauth?redirect_uri=" in headers['Location']:
+            formated_cookies = ';'.join( [cookie.name+"="+cookie.value for cookie in cookies if cookie.name in ['datr', 'c_user', 'csm', 'fr', 'lu', 's', 'xs']] )
+            redirect_url = headers['Location']
+
+            redirect_req = urllib2.Request(redirect_url)
+            add_proper_headers(redirect_req, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "www.google.fr", formated_cookies )
+            resp = urllib2.urlopen(redirect_req)
+
+            access_token = re.findall('access_token=(.*)&expires_in', resp.read())[0]
+            user_id = re.findall('c_user=([0-9]+);', formated_cookies)[0]
+            return resp
+        return urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
+            
+
+
+
+cookies = cookielib.CookieJar()
+handlers = [urllib2.HTTPHandler(), urllib2.HTTPSHandler(), urllib2.HTTPCookieProcessor(cookies), MyHTTPRedirectHandler]
+opener = urllib2.build_opener(*handlers)
+urllib2.install_opener(opener)
+
+
+
+def add_proper_headers(http_req, accept, referer="", cookie = ""):
+    http_req.add_header('User-Agent', "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0")
+    http_req.add_header('Accept', accept)
+    http_req.add_header('Accept-Language', "fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+    http_req.add_header('Connection', "keep-alive")
+    if referer != "":
+        http_req.add_header('Referer', referer)
+    if cookie != "":
+        http_req.add_header('Cookie', cookie)
+
+
+def get_post_params():
+    login_get = "https://www.facebook.com/login.php?skip_api_login=1&api_key=210827375150&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fredirect_uri%3Dhttp%253A%252F%252Fstatic.ak.facebook.com%252Fconnect%252Fxd_arbiter.php%253Fversion%253D28%2523cb%253Dfbad4334335328%2526domain%253Dwww.shazam.com%2526origin%253Dhttp%25253A%25252F%25252Fwww.shazam.com%25252Ff2708561c4c729%2526relation%253Dopener%2526frame%253Df2fc871b8f8b862%26display%3Dpopup%26scope%3Demail%252Cpublish_actions%26response_type%3Dtoken%252Csigned_request%26domain%3Dwww.shazam.com%26client_id%3D210827375150%26ret%3Dlogin%26sdk%3Djoey&cancel_uri=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D28%23cb%3Dfbad4334335328%26domain%3Dwww.shazam.com%26origin%3Dhttp%253A%252F%252Fwww.shazam.com%252Ff2708561c4c729%26relation%3Dopener%26frame%3Df2fc871b8f8b862%26error%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26e2e%3D%257B%257D&display=popup"
+
+    resp = urllib2.urlopen(login_get)
+    resp_content = resp.read()
+    formated_cookies = ';'.join([cookie.name+"="+cookie.value for cookie in cookies])
+    param_lsd = re.findall('name="lsd" value="(.*)" autocomplete="off" />', resp_content)[0]
+    param_lgnrnd = re.findall('name="lgnrnd" value="(.*)" /><input type', resp_content)[0]
+    #param_next = re.findall('name="next" value="(.*)"', resp_content)[0]
+    login_post_url = html_parser.unescape("https://www.facebook.com/" + re.findall('action="/(login.php\?login_attempt=1.*)" method="post"', resp_content)[0])
+    #return login_post_url, formated_cookies, param_lsd, param_lgnrnd, param_next
+    return login_post_url, formated_cookies, param_lsd, param_lgnrnd
+
+
+def get_api_cookie(login, password):
+    global cookies
+
+    #login_post_url, formated_cookies, param_lsd, param_lgnrnd, param_next = get_post_params()
+    login_post_url, formated_cookies, param_lsd, param_lgnrnd = get_post_params()
+
+    post_data = { 'lsd':param_lsd,
+                  'api_key':'210827375150',
+                  'display':'popup',
+                  'enable_profile_selector':'',
+                  'legacy_return':'1',
+                  #'next' : html_parser.unescape(param_next), 
+                  'profile_selector_ids':'',
+                  'skip_api_login':'1',
+                  'signed_next':'1',
+                  'trynum':'1',
+                  'timezone':'',
+                  'lgnrnd':param_lgnrnd,
+                  'lgnjs':'n',
+                  'email':login,
+                  'pass':password,
+                  'default_persistent':'0',
+                  'login':'Connexion' }
+
+    post_data = urllib.urlencode(post_data)
+    req = urllib2.Request(login_post_url)
+    add_proper_headers(req, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", cookie=formated_cookies )
+    resp = urllib2.urlopen(req, post_data)
+    return user_id, access_token
+
+
